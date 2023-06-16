fa8d91a21a902e78dabdc9cce60cab8aa5193738
Arkar Chan Myae <arkarcham20@gmail.com>
2023-06-16 23:28:22 +0630

    Stopwatch - gives camelCase names and fix mode-2 btn

--- app.js
+++ app.js
@@ -1,92 +1,114 @@
-const getmodebtn = document.querySelector('.mode-btn');
-const getdisplays = document.querySelectorAll('.display');
-const getmilli = document.querySelector(".milli");
-const getstartbtn = document.querySelector('.start'),
-        getpausebtn = document.querySelector('.pause'),
-        getresetbtn = document.querySelector('.reset');
-const getdlpsec = document.getElementById('dlpsecond'),
-	    getdlpmlsec = document.getElementById('dplmillisec');
+const getModebtn = document.querySelector('.mode-btn');
+const getDisplays = document.querySelectorAll('.display');
+const getMilli = document.querySelector(".milli");
+const getStartbtn = document.querySelector('.start'),
+    getPausebtn = document.querySelector('.pause'),
+    getResetbtn = document.querySelector('.reset');
+const getDplsec = document.getElementById('dlpsecond'),
+	getDplmlisec = document.getElementById('dplmillisec');
 
 let idx = 0;
 
-let setinterdisplay;
+let setIntervalDisplay;
 
 let [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
 
-getdisplays[idx].style.display = "block";
+getDisplays[idx].style.display = "block";
 
-getmodebtn.addEventListener("click", () => {
-    getdisplays[idx].style.display = "none";
-    reloadagain();
+getModebtn.addEventListener("click", () => {
+    getDisplays[idx].style.display = "none";
+    reloadAgain();
+
     idx++;
+
     if (idx > 1) {
         idx = 0;
     }
-    getdisplays[idx].style.display = "block";
-    getmodebtn.innerHTML = `<i class="fas fa-tools"></i> Mode ${idx + 1}`;
+
+    getDisplays[idx].style.display = "block";
+    getModebtn.innerHTML = `<i class="fas fa-tools"></i> Mode ${idx + 1}`;
 })
 
-getstartbtn.onclick = () => {
-    clearInterval(setinterdisplay);
-    setinterdisplay= setInterval(displaytime,10);
+getStartbtn.onclick = () => {
+    clearInterval(setIntervalDisplay);
+    setIntervalDisplay= setInterval(displayTime,10);
 }
 
-getpausebtn.onclick = () => {
-    clearInterval(setinterdisplay);
+getPausebtn.onclick = () => {
+    clearInterval(setIntervalDisplay);
 }
 
-getresetbtn.onclick = () => {
-    reloadagain();
+getResetbtn.onclick = () => {
+    reloadAgain();
 }
 
-function reloadagain() {
+function reloadAgain() {
     [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
-    clearInterval(setinterdisplay);
+    clearInterval(setIntervalDisplay);
+
     if (idx === 0) {
-        getdisplays[idx].innerHTML = "00 : 00 : 00";
-        getmilli.setAttribute("data-test","000");
+        getDisplays[idx].innerHTML = "00 : 00 : 00";
+        getMilli.setAttribute("data-test", "000");
     } else {
-        getdlpmlsec.innerHTML = "0" + milliseconds;
-        getdlpsec.textContent = "0" + seconds;
+        getDplmlisec.innerHTML = "0" + milliseconds;
+        getDplsec.textContent = "0" + seconds;
     }
 }
 
-function displaytime() {
+function displayTime() {
 
     if (idx === 0) {
+
         milliseconds += 10;
+
         if (milliseconds === 1000) {
+
             seconds++;
             milliseconds = 0;
+
             if (seconds === 60) {
+
                 minutes++
                 seconds = 0;
+
                 if (minutes === 60) {
+
                     hours++;
                     minutes = 0;
+
                 }
             }
         }
 
-        const hourtext = hours.toString().padStart(2, "0");
-        const minutetext = minutes.toString().padStart(2, "0");
-        const secondtext = seconds.toString().padStart(2, "0");
-        const millitext = milliseconds.toString().padStart(3, "0");
-        getdisplays[idx].innerHTML = `${hourtext} : ${minutetext} : ${secondtext}`;
-        getmilli.setAttribute("data-test",millitext);
+        const hourText = hours.toString().padStart(2, "0");
+        const minuteText = minutes.toString().padStart(2, "0");
+        const secondText = seconds.toString().padStart(2, "0");
+        const milliText = milliseconds.toString().padStart(3, "0");
+
+        getDisplays[idx].innerHTML = `${hourText} : ${minuteText} : ${secondText}`;
+        getMilli.setAttribute("data-test",milliText);
 
     } else {
 
         milliseconds++;
-        if (milliseconds < 9) {
-            getdlpmlsec.innerHTML = "0" + milliseconds;
-        } else if (milliseconds > 99) {
-            seconds++;
-            milliseconds = 0;
-            getdlpsec.innerHTML = "0" + seconds;
-        } else {
-            getdlpmlsec.textContent = milliseconds;
-        }
+
+		if(milliseconds <= 9){
+			getDplmlisec.textContent = "0" + milliseconds;
+		}
+
+		if(milliseconds > 9){
+			getDplmlisec.innerText = milliseconds;
+		}
+
+		if(milliseconds > 99){
+			milliseconds = 0;
+			seconds++;
+			getDplsec.textContent = "0" + seconds;
+		}
+
+		if(seconds > 9){
+			getDplsec.innerText = seconds;
+		}
 
     }
 }
